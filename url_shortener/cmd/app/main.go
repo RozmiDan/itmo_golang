e557b67fa5b472335ee9505e5efb59f561fadaee
Daniel <rozmirski@mail.ru>
2025-03-03 23:30:07 +0300

    add db

--- url_shortener/cmd/app/main.go
+++ url_shortener/cmd/app/main.go
@@ -2,15 +2,29 @@ package main
 
 import (
 	"fmt"
+	"os"
 
 	"github.com/RozmiDan/url_shortener/internal/config"
+	"github.com/RozmiDan/url_shortener/internal/storage/sqlite"
 	"github.com/RozmiDan/url_shortener/pkg/logger"
 )
 
 func main() {
-	cnfg := config.MustLoad();
+	cnfg := config.MustLoad()
 	logger := logger.NewLogger(cnfg.Env)
-	fmt.Println("helo")
-	logger.Debug("Hello")
-	fmt.Println(cnfg)
+
+	logger.Info("url-shortner started")
+	logger.Debug("debug mode")
+
+	storage, err := sqlite.New(cnfg.StoragePath)
+	if err != nil {
+		logger.Error(err.Error())
+		os.Exit(1)
+	}
+	res, err := storage.GetURL("google")
+	if err != nil {
+		logger.Error(err.Error())
+	}
+	fmt.Println(res)
+
 }
